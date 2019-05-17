b93dcdbaea18b13bab492aa47acbb8a874ddc9aa
Abdullah Al Noman <noman.sliit@gmail.com>
2019-05-17 05:51:11 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- 
+Name: Abdullah Al Noman
+Student ID: IT17155908
+-->
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="style.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<title>Double Submit Cookie Pattern</title>
+	</head>
+	<body>
+        <div class="bg">
+	<h1 id="title">CSRF - Double Submit Cokkie Pattern </h1>
+		<div class="container">
+        <div id="login-row" class="row justify-content-center align-items-center">
+            <div id="login-column" class="col-md-6">
+                <div class="box">
+                    <div class="float">
+                        <form class="form" action="result.php" method="post">
+                            <div class="form-group">
+                                <label for="username" class="text-white">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-white">Password:</label><br>
+                                <input type="password" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="Login" class="btn btn-info btn-md" value="Login">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+	</body> 
+</html>
